5e4e2b44766e3db6c7a3871ca1ec6eab8b1f0859
amitprasad618 <amitprasad618@gmail.com>
2025-12-27 12:22:37 +0530

    fetch data by Id and see sorted order data through api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -19,5 +19,37 @@ def view():
     data = load_data()
     return data
 
-patients = load_data()
-print(patients[0]["name"])
+@app.get('/view/{patient_id}')
+def viewByID(patient_id: int = Path(..., description='Id of the patient', example=3)):  #... mtlb require hai
+    #load all the patient
+    data = load_data()
+
+    for patient in data:
+        if patient["id"] == patient_id:
+            return patient
+
+    raise HTTPException(
+        status_code=404,
+        detail="Data does not exist in database"
+    )
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the bases of id or age'), order: str = Query('asc', description='sort in asc or desc order')):
+    valid_field = ['age', 'id']
+
+    if sort_by not in valid_field:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid field selected from {valid_field}"
+        )
+    if order not in ['asc', 'desc']:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid order selected. Please select order between asc and desc"
+        ) 
+    data = load_data()
+
+    reverse = True if order == 'desc' else False
+    sorted_data = sorted(data, key=lambda x: x.get(sort_by, 0), reverse=reverse)
+
+    return sorted_data
